4ddc675c9081603ef4e98803fa220ab98ac60a57
Manuel Reinhardt <reinhardt@syslab.com>
2018-03-05 08:51:00 +0000

    Custom PDF cover logo. Refs #16527

--- recensio/regio/browser/views.py
+++ recensio/regio/browser/views.py
@@ -1,4 +1,11 @@
 from recensio.theme.browser.homepage import HomepageView
+from recensio.theme.browser.pdfgen import GeneratePdfRecension
+
+
+class GeneratePdfRecensionRegio(GeneratePdfRecension):
+    """Customized cover page
+    """
+    logo_main = '++resource++recensio.regio.images/logo2_fuer-Deckblatt.jpg'
 
 
 class RegioHomepageView(HomepageView):
