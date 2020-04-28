a3a35261b2a66130aa1636c671bdd05d6a84af25
Manuel Reinhardt <reinhardt@syslab.com>
2020-04-28 09:44:56 +0200

    Black

--- recensio/regio/browser/views.py
+++ recensio/regio/browser/views.py
@@ -5,9 +5,10 @@ from recensio.theme.browser.pdfgen import GeneratePdfRecension
 class GeneratePdfRecensionRegio(GeneratePdfRecension):
     """Customized cover page
     """
-    logo_main = '++resource++recensio.regio.images/logo2_fuer-Deckblatt.jpg'
+
+    logo_main = "++resource++recensio.regio.images/logo2_fuer-Deckblatt.jpg"
 
 
 class RegioHomepageView(HomepageView):
 
-    review_languages = [u'']
+    review_languages = [u""]
--- setup.py
+++ setup.py
@@ -1,37 +1,26 @@
 from setuptools import setup, find_packages
 
-version = '1.3.1.dev0'
+version = "1.3.1.dev0"
 
-setup(name='recensio.regio',
-      version=version,
-      description="Regio customizations for Recensio",
-      long_description=(open("README.txt").read() + "\n" +
-                        open("CHANGES.txt").read()),
-      classifiers=[
-          "Framework :: Plone",
-          "Programming Language :: Python :: 2.7",
-          ],
-      keywords='web zope plone theme',
-      author='Syslab.com GmbH',
-      author_email='info@syslab.com',
-      url='https://github.com/syslabcom/recensio.regio/',
-      license='GPL',
-      packages=['vocabularies'] + find_packages(exclude=['ez_setup']),
-      namespace_packages=['recensio'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'recensio.policy',
-          'recensio.theme',
-          'z3c.jbot',
-          'setuptools',
-      ],
-      extras_require={
-          "test": ["plone.app.testing",
-                   ],
-      },
-      entry_points="""
+setup(
+    name="recensio.regio",
+    version=version,
+    description="Regio customizations for Recensio",
+    long_description=(open("README.txt").read() + "\n" + open("CHANGES.txt").read()),
+    classifiers=["Framework :: Plone", "Programming Language :: Python :: 2.7",],
+    keywords="web zope plone theme",
+    author="Syslab.com GmbH",
+    author_email="info@syslab.com",
+    url="https://github.com/syslabcom/recensio.regio/",
+    license="GPL",
+    packages=["vocabularies"] + find_packages(exclude=["ez_setup"]),
+    namespace_packages=["recensio"],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=["recensio.policy", "recensio.theme", "z3c.jbot", "setuptools",],
+    extras_require={"test": ["plone.app.testing",],},
+    entry_points="""
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      )
+)
