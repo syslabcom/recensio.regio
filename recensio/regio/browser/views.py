dfe21329a226eedc6c82a539d413ab74997958f9
Manuel Reinhardt <reinhardt@syslab.com>
2024-04-25 14:43:29 +0200

    Custom ddcPlace vocabulary
    Fixes syslabcom/scrum#2194

--- recensio/regio/browser/views.py
+++ recensio/regio/browser/views.py
@@ -1,3 +1,4 @@
+from recensio.plone.browser.helper import VocabularyHelper
 from recensio.plone.browser.homepage import HomepageView
 from recensio.plone.browser.pdfgen import GeneratePdfRecension
 
@@ -12,3 +13,8 @@ class RegioHomepageView(HomepageView):
 
     resource_directory = "++resource++recensio.regio.images"
     review_languages = [u""]
+
+class RegioVocabularyHelper(VocabularyHelper):
+    @property
+    def ddcPlace(self):
+        return self.get_named_vocabulary("recensio.regio.vocabularies.region_values")
